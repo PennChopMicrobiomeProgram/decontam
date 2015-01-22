85c2639c6de959101e1d481b831776743f9c6faf
Anatoly Dryga <anatolydryga@gmail.com>
2015-01-22 17:29:08 -0500

    test file with samples information

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -18,13 +18,20 @@ class Test_Tools(unittest.TestCase):
 class  Test_Samples(unittest.TestCase):
     
     def test_empty_file(self):
-        self.assertTrue(False)
+        self.assertRaises(IOError, parser.parse_samples, StringIO(""))
 
     def test_incorrect_file_format(self):
-        self.assertTrue(False)
+        self.assertRaises(IOError, parser.parse_samples, StringIO("name1\tfastq\n"))
 
     def test_file_does_not_exist(self):
-        self.assertTrue(False)
+        parser.os.path.isfile = lambda _ : False 
+        self.assertRaises(IOError, parser.parse_samples, 
+            StringIO("name1\tAbsent_fastq\tAbsent_annotation\n"))
 
     def test_valid_file(self):
-        self.assertTrue(False)
+        parser.os.path.isfile = lambda _ : True 
+        samples = parser.parse_samples(
+            StringIO("name1\tfastq1\tannotation1\nname2\tfastq2\tannotation2\n"))
+        self.assertEqual(len(samples), 2)
+        self.assertEqual(samples[0], ("name1", "fastq1", "annotation1"))
+        self.assertEqual(samples[1], ("name2", "fastq2", "annotation2"))
