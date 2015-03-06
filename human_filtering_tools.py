d2144c61ea92363a2c7c0d44adcb34703fc920e1
Anatoly Dryga <anatolydryga@gmail.com>
2015-03-05 21:25:28 -0500

    factory for list of tools creation

--- human_filtering_tools.py
+++ human_filtering_tools.py
@@ -2,10 +2,24 @@ class Bmtagger:
     def __init__(self):
         pass
 
+    def get_human_reads(self, fastq):
+        pass
+
+    def get_name(self):
+        pass
+
 class All_human:
     def __init__(self):
+        pass
+    def get_human_reads(self, fastq):
+        pass
+    def get_name(self):
         pass
 
 class None_human:
     def __init__(self):
+        pass
+    def get_human_reads(self, fastq):
+        pass
+    def get_name(self):
         pass
