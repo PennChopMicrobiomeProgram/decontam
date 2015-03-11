aee39bf544de4f6916cb230723592becea39d5fe
Anatoly Dryga <anatolydryga@gmail.com>
2015-03-11 13:44:13 -0400

    implmentation of factory of tools and parameters for tools

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,4 +1,8 @@
 import unittest
+import mock
+import tempfile
+import json
+from cStringIO import StringIO
 
 from .. import tools
 from ..human_filtering_tools import Bmtagger
@@ -6,21 +10,48 @@ from ..human_filtering_tools import All_human
 from ..human_filtering_tools import None_human
 
 class Test_tools_creation(unittest.TestCase):
+    
+    def setUp(self):
+        self.sample_json_fh = StringIO('{"bmtagger" : {"bitmask" : "path"}}')        
+        self.params = json.load(self.sample_json_fh)
 
     def test_empty_tool_names(self):
-        self.assertRaises(ValueError, tools.create_tools, [])
+        self.assertRaises(ValueError, tools.create_tools, [], self.params)
 
     def test_unknown_tool(self):
         tool_names = ["unknown_and_never_existing_tool"]
-        self.assertRaises(ValueError, tools.create_tools, tool_names)
+        self.assertRaises(ValueError, tools.create_tools, tool_names, self.params)
 
     def test_can_create_tools(self):
         tool_names = [ "bmtagger", "all_human", "none_human" ]
-        tool_runners = tools.create_tools(tool_names) 
+        tool_runners = tools.create_tools(tool_names, self.params) 
         self.assertEqual(len(tool_runners), len(tool_names))
         self.assertTrue(isinstance(tool_runners[0], Bmtagger))
         self.assertTrue(isinstance(tool_runners[1], All_human))
         self.assertTrue(isinstance(tool_runners[2], None_human))
 
+class Test_parameter_file_default(unittest.TestCase):
 
+    def setUp(self):
+        self.real_function = tools.json.load
+        sample_json_fh = StringIO('{"bmtagger_DEF" : {"bitmask_DEF" : "path"}}')        
+        params = json.load(sample_json_fh)
+        tools.json.load = mock.Mock(return_value = params)
 
+    def tearDown(self):
+        tools.json.load = self.real_function
+
+    def test_read_json_default(self):
+        parameters = tools.get_parameters_for_tools()
+        self.assertTrue(parameters["bmtagger_DEF"] is not None)
+        self.assertTrue(parameters["bmtagger_DEF"]["bitmask_DEF"] is not None)
+
+class Test_parameter_file(unittest.TestCase):
+    
+    def test_read_json(self):
+        sample_json = tempfile.NamedTemporaryFile()        
+        sample_json.write('{"bmtagger" : {"bitmask" : "path"}}')        
+        sample_json.seek(0)
+        parameters = tools.get_parameters_for_tools(sample_json.name)
+        self.assertTrue(parameters["bmtagger"] is not None)
+        self.assertTrue(parameters["bmtagger"]["bitmask"] is not None)
--- tools.py
+++ tools.py
@@ -1,27 +1,48 @@
+import json
+
 from human_filtering_tools import Bmtagger
 from human_filtering_tools import All_human
 from human_filtering_tools import None_human
+from human_filtering_tools import Random_human
 
 """ creates list of tools that can process the input file.
-    each tool implements extract_human_reads() method
+    each tool implements get_human_annotation() method and has name attribute
+    and __init__ with one argument.
 """
 
-toolname_to_runner = {
-    "bmtagger": Bmtagger(),
-    "all_human": All_human(),
-    "none_human": None_human()
+tools_available = {
+    Bmtagger,
+    All_human,
+    None_human, 
+    Random_human
 }
 
-
-def check_if_valid_tool(tool_name):
+def check_if_valid_tool(tool_name, toolname_to_runner):
     if not tool_name in toolname_to_runner:
         raise ValueError("Tool: " + tool_name + 
             " is not a valid tool; change tool name or add it to map of tools(toolname_to_runner)")
 
-
-def create_tools(tool_names):
+def get_parameters_for_tools(path_to_parameter_file="parameters.json"):
+    """parse json file with parameters for tools.
+       
+    Agrs:
+       path_to_params path to json file
+       
+    Raises:
+        IOError if cannot find or parse file. 
     """
-        for each tool from from the tool_names list checks that tool is valid and create tool
+    params = json.load(open(path_to_parameter_file))
+    return params
+
+
+def create_tools(tool_names, tool_parameters):
+    """
+        create tools for running human filtering.
+
+        For each tool from from the tool_names list checks that tool is valid and create tool
+        using tool_parameters dictionary.
+
+        note: tool_parameters ..
 
         return list of tools(each tool implement runner and parser)
 
@@ -30,9 +51,10 @@ def create_tools(tool_names):
         raises ValueError if tool is not known
     """
     tools = []
+    toolname_to_runner = {tool.name : tool for tool in tools_available}
     if not tool_names:
         raise ValueError("empty tools list; need at least one tool")
     for tool_name in tool_names:
-        check_if_valid_tool(tool_name)
-        tools.append(toolname_to_runner[tool_name])
+        check_if_valid_tool(tool_name, toolname_to_runner)
+        tools.append(toolname_to_runner[tool_name](tool_parameters.get(tool_name, None)))
     return tools
