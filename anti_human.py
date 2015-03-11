ae01d5205d3d3a586efd23d006fab40d3700ce19
Anatoly Dryga <anatolydryga@gmail.com>
2015-03-11 13:46:30 -0400

    implemented tool runners for samples and saving results into file

--- anti_human.py
+++ anti_human.py
@@ -3,6 +3,11 @@
 import argparse
 import os.path
 import sys
+import csv
+
+import parser
+import tools
+import utils
 
 def check_file_exists_or_die(file_name):
     if not os.path.isfile(file_name): 
@@ -11,44 +16,58 @@ def check_file_exists_or_die(file_name):
 
 def command_line_arguments():
     """creates command-line args.
-    returns arguments as attributes
+    Returns:
+        arguments as attributes
     """
-    parser = argparse.ArgumentParser(description="Compares performance of human filtering tools.")
-    parser.add_argument("-s", "--samples", required=True, type=str, help="list of samples and corresponding annotation files")
-    parser.add_argument("-t", "--tools", required=True, type=str, help="human filtering tools")
-    parser.add_argument("-o", "--output", required=True, help="comparison of tools to expected results.")
+    parser = argparse.ArgumentParser(description="Run human filtering tools.")
+    parser.add_argument("-s", "--samples", required=True, type=str, help="list of paired-end samples.")
+    parser.add_argument("-t", "--tools", required=True, type=str, help="human filtering tools.")
+    parser.add_argument("-p", "--parameters_for_tools", 
+        help="parameters for different tools(by default search parameters.json file in current folder.)")
+    parser.add_argument("-o", "--output", required=True, help="long table of results.")
     args = parser.parse_args()
     check_file_exists_or_die(args.samples) 
     check_file_exists_or_die(args.tools) 
     return args
 
+def write_results(filename, results):
+    """ write tool, sample, read_id, is_human to tab-separated file.
+    """
+    writer = csv.writer(open(filename, 'w'), delimiter="\t")
+    writer.writerow(["tool", "sample", "read_id", "is_human"])
+    writer.writerows(results)
+  
+
 if __name__=="__main__":
     args = command_line_arguments()
 
-    tool_names = parse_tool_names(open(args.tools)) 
-    tools = Tools.create(tool_names)
+    tool_names = parser.parse_tool_names(open(args.tools)) 
+    if args.parameters_for_tools:
+        tool_parameters = tools.get_parameters_for_tools(args.parameters_for_tools)
+    else:
+        tool_parameters = tools.get_parameters_for_tools()
+    tools = tools.create_tools(tool_names, tool_parameters)
 
-    samples = parse_samples(args.samples)
+    samples = parser.parse_samples(open(args.samples))
 
     results = []
 
     for sample in samples:
-        sample_name = sample[0]
-        fastq_file = sample[1]
-        annotation_file = sample[2]
+        (sample_name, R1_fastq_file, R2_fastq_file) = sample
 
-        annotation_file_handle = open(annotation_file)
-        annotation = parse_annotation(annotation_file_handle)
-        fastq_file_handle = open(fastq_file)
-        reads = parse_reads(fastq_file_handle)
-        check_all_reads_are_annotated(annotation, reads)
+        R1_read_ids = utils.parse_read_ids(R1_fastq_file)
+        R2_read_ids = utils.parse_read_ids(R2_fastq_file)
+        if not utils.check_all_read_ids_are_consistent(R1_read_ids, R2_read_ids):
+            print "R1 and R2 file should have the same ids."
+            print "R1  file  " + R1_fastq_file + " R2 file " + R2_fastq_file + " are not consistent."
+            sys.exit(1)
+
         for tool in tools:
-            predicted_human = tool.get_human_reads(sample_name)
-            tool_name = tool.get_name()
-#            quality_metrics = check_tool_vs_oracle(predicted_human, annotation)
-#            results.append( (sample_name, tool_name, quality_metrics) )
-#
-#    write_results(results)
+            human_annotation = tool.get_human_annotation(R1_fastq_file, R2_fastq_file)
+            tool_name = tool.name
+            results_for_tool_sample = utils.add_tool_sample(tool_name, sample_name, human_annotation)
+            results += results_for_tool_sample
+    write_results(args.output, results)
              
  
     
