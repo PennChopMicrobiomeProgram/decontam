b05e3b85a996081d5525251276f3bef87d1ed6b1
Anatoly Dryga <anatolydryga@gmail.com>
2015-03-24 11:36:33 -0400

    bowtie tool is added

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -8,11 +8,14 @@ from .. import tools
 from ..human_filtering_tools import Bmtagger
 from ..human_filtering_tools import All_human
 from ..human_filtering_tools import None_human
+from ..human_filtering_tools import Random_human
+from ..human_filtering_tools import Bowtie
 
 class Test_tools_creation(unittest.TestCase):
     
     def setUp(self):
-        self.sample_json_fh = StringIO('{"bmtagger" : {"bitmask" : "path"}}')        
+        self.sample_json_fh = StringIO(
+            '{"bmtagger" : {"bitmask" : "path"}, "bowtie" : {"index" : "path2"}, "random_human" : {"percent_human" : 30}}')        
         self.params = json.load(self.sample_json_fh)
 
     def test_empty_tool_names(self):
@@ -23,12 +26,14 @@ class Test_tools_creation(unittest.TestCase):
         self.assertRaises(ValueError, tools.create_tools, tool_names, self.params)
 
     def test_can_create_tools(self):
-        tool_names = [ "bmtagger", "all_human", "none_human" ]
+        tool_names = [ "bmtagger", "all_human", "none_human", "random_human", "bowtie" ]
         tool_runners = tools.create_tools(tool_names, self.params) 
         self.assertEqual(len(tool_runners), len(tool_names))
         self.assertTrue(isinstance(tool_runners[0], Bmtagger))
         self.assertTrue(isinstance(tool_runners[1], All_human))
         self.assertTrue(isinstance(tool_runners[2], None_human))
+        self.assertTrue(isinstance(tool_runners[3], Random_human))
+        self.assertTrue(isinstance(tool_runners[4], Bowtie))
 
 class Test_parameter_file_default(unittest.TestCase):
 
