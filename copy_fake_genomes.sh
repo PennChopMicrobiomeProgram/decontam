8cf397f1971bba67ea612be761bd4282c8ac7630
Kyle Bittinger <kylebittinger@gmail.com>
2015-06-09 16:40:58 -0400

    Removed unused files.

--- copy_fake_genomes.sh
+++
@@ -1,9 +0,0 @@
-rm samples_fake_genomes.txt
-
-for folder in  /home/ashwini/ash/synthetic-fastq/fake_genome*; do
-    echo $folder
-    name=${folder##*fake_genome}
-    cp $folder/fake_genome-R1.fastq fake_genome_${name}_R1.fastq
-    cp $folder/fake_genome-R2.fastq fake_genome_${name}_R2.fastq
-    echo fake_genome_${name} fake_genome_${name}_R1.fastq fake_genome_${name}_R2.fastq >> samples_fake_genomes.txt
-done
