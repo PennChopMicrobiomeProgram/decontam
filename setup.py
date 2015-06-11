160e416971bd432bcdf09acd32106948e9003e65
Kyle Bittinger <kylebittinger@gmail.com>
2015-06-11 11:27:36 -0400

    Renamed script with more general name.

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
     author_email='kylebittinger@gmail.com',
     url='https://github.com/PennChopMicrobiomeProgram',
     packages=['decontamlib'],
-    scripts=['scripts/anti_human.py', 'scripts/make_index.py'],
+    scripts=['scripts/decontaminate.py', 'scripts/make_index.py'],
     )
