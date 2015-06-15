f9d759c1f012c52b82ce29aea3820378deaffeab
Kyle Bittinger <kylebittinger@gmail.com>
2015-06-15 12:12:44 -0400

    Moved FASTQ related code to separate file.

---
+++ decontamlib/fastq.py
@@ -0,0 +1,66 @@
+import itertools
+import os
+
+class FastqSplitter(object):
+    suffixes = {
+        True: "_human",
+        False: "",
+        }
+
+    def __init__(self, input_fp, output_dir):
+        self.output_dir = output_dir
+        self.input_fp = input_fp
+        input_filename = os.path.basename(input_fp)
+        self.input_root, self.input_ext = os.path.splitext(input_filename)
+        self._open_files = {}
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.close()
+
+    def partition(self, annotations):
+        ids_to_annotation = dict(annotations)
+        with open(self.input_fp) as f:
+            for desc, seq, qual in parse_fastq(f):
+                read_id = desc.split()[0]
+                annotation = ids_to_annotation[read_id]
+                self._write((desc, seq, qual), annotation)
+
+    def _write(self, read, annotation):
+        desc, seq, qual = read
+        suffix = self.suffixes[annotation]
+        output_filename = self.input_root + suffix + self.input_ext
+        fp = os.path.join(self.output_dir, output_filename)
+        if fp not in self._open_files:
+            self._open_files[fp] = open(fp, "w")
+        f = self._open_files[fp]
+        write_fastq(f, desc, seq, qual)
+
+    def close(self):
+        for f in self._open_files.values():
+            f.close()
+
+
+def _grouper(iterable, n):
+    "Collect data into fixed-length chunks or blocks"
+    args = [iter(iterable)] * n
+    return itertools.izip(*args)
+
+
+def parse_fastq(f):
+    """ parse original fastq file and write new fastq file the filtered non-human reads.
+    """
+    for desc, seq, _, qual in _grouper(f, 4):
+        desc = desc.rstrip()[1:]
+        seq = seq.rstrip()
+        qual = qual.rstrip()
+        yield desc, seq, qual
+
+
+def write_fastq(out_fastq, desc, seq, qual):
+    out_fastq.write("@" + desc + "\n")
+    out_fastq.write(seq + "\n")
+    out_fastq.write("+\n")
+    out_fastq.write(qual + "\n")
