3e62942bc45dfbcc76954534ccf4a4c5fc8fb73a
ctanes <ctanes@gmail.com>
2015-07-13 18:04:09 -0400

    Changed default config so that the nosetests dn't fail

--- decontamlib/main.py
+++ decontamlib/main.py
@@ -13,8 +13,8 @@ from decontamlib.tools import FilteringTool
 
 default_config = {
     "method": "bowtie2",
-    "bowtie2_fp": "/home/kyle/software/bowtie2-2.2.5/bowtie2",
-    "bwa_fp": "/home/kyle/software/bwa-0.7.12/bwa",
+    "bowtie2_fp": "bowtie2",
+    "bwa_fp":"bwa"
     }
 
 def human_filter_main(argv=None):
